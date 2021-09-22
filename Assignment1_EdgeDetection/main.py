46e1a0a1d6f4dcf9b7ba8c497ff9ff3f15b7c7a7
Karl <karlsheng99>
2021-09-22 01:35:55 -0400

    update files

--- Assignment1_EdgeDetection/main.py
+++ Assignment1_EdgeDetection/main.py
@@ -1,8 +1,21 @@
+import numpy as np
 from PIL import Image
+import gaussian
+import convolution
+
 
 def main():
-    image = Image.open('x.png')
-    image.show()
+    im = Image.open('images/kangaroo.pgm')
+    im.show()
+    im.save('in.png')
+    source = np.array(im)
+    im.close()
+    kernel = gaussian.gaussian_filter(1)
+    out_pix = convolution.convolve(source, kernel)
+    out_pic = Image.fromarray(out_pix)
+    out_pic.show()
+    out_pic.convert("L").save('out.png')
+
 
 if __name__ == '__main__':
     main()
