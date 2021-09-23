531c0e464a0724c4880e99daeb3f2a601cd3e355
karlsheng99 <karlsheng99@gmail.com>
2021-09-23 12:27:09 -0400

    update main.py

--- Assignment1_EdgeDetection/main.py
+++ Assignment1_EdgeDetection/main.py
@@ -1,29 +1,50 @@
 import sys
-import os.path
-from os import path
-import numpy as np
+from pathlib import Path
 from PIL import Image
+import numpy as np
 import gaussian
 import convolution
 import gradient
 import suppression
 
 
+def edge_detection(image, sigma, threshold):
+    source_path = 'images/' + image
+    source_pic = Image.open(source_path)
+    result_dir = 'results/'
+    if not Path(result_dir).exists():
+        Path(result_dir).mkdir()
+    image_name = image[:-4]
+    source_pic.save(result_dir + image_name + '.png')
 
+    source_pix = np.array(source_pic)
+    source_pic.close()
 
-def edge_detection():
-    threshold = 80
-    im = Image.open('images/kangaroo.pgm')
-    im.show()
-    # im.save('in.png')
-    source = np.array(im)
-    im.close()
-    kernel = gaussian.gaussian_filter(1)
-    smoothed_pix = convolution.convolve(source, kernel)
+    """
+    sigma = 1 -> sigma=1 * 1
+    sigma = 2 -> sigma=1 * 3
+    sigma = 4 -> sigma=1 * 5
+    sigma = 8 -> sigma=1 * 7
+    """
+    smoothed_pix = np.copy(source_pix)
+    if sigma == 1 or sigma == 2 or sigma == 4 or sigma == 8:
+        gauss_kernel = gaussian.gaussian_filter(1)
+        for i in range(int(np.log2(sigma)) * 2 + 1):
+            smoothed_pix = convolution.convolve(smoothed_pix, gauss_kernel)
+    else:
+        gauss_kernel = gaussian.gaussian_filter(sigma)
+        smoothed_pix = convolution.convolve(smoothed_pix, gauss_kernel)
+
+    smoothed_pic = Image.fromarray(smoothed_pix)
+    smoothed_pic.convert("L").save(result_dir + image_name + '_gaussian_sigma=' + str(sigma) + '.png')
+
     gradient_magnitude, gradient_direction = gradient.compute_gradient(smoothed_pix, threshold)
-    out_pix = suppression.non_max_suppress(gradient_magnitude, gradient_direction)
-    out_pic = Image.fromarray(out_pix)
-    out_pic.show()
+    gradient_pic = Image.fromarray(gradient_magnitude)
+    gradient_pic.convert("L").save(result_dir + image_name + '_gradient_sigma=' + str(sigma) + '_threshold=' + str(threshold) + '.png')
+
+    suppression_pix = suppression.non_max_suppress(gradient_magnitude, gradient_direction)
+    suppression_pic = Image.fromarray(suppression_pix)
+    suppression_pic.convert("L").save(result_dir + image_name + '_suppression_sigma=' + str(sigma) + '_threshold=' + str(threshold) + '.png')
 
 
 def main():
@@ -44,22 +65,24 @@ def main():
         -pledge
     """
 
-    image_path = "images/red.pgm"
-    sigma = 1
+    filename = 'red.pgm'
+    sigma = 4
     threshold = 80
 
-    if len(sys.argv) == 1:
-        print("Run:", sys.argv[0], "file='images/red.pgm' sigma=1 threshold=80 by default")
-    elif len(sys.argv) >= 2:
-        file_path = "images/" + sys.argv[1]
-        if not path.exists(file_path):
-            print("Error: file does not exist")
-            return 1
-        else:
-            image_path = file_path
+    edge_detection(filename, sigma, threshold)
 
-        if len(sys.argv) == 2:
-            print("Run:", sys.argv[0], sys.argv[1], "sigma=1 threshold=80 by default")
+    # if len(sys.argv) == 1:
+    #     print("Run:", sys.argv[0], "file='images/red.pgm' sigma=1 threshold=80 by default")
+    # elif len(sys.argv) >= 2:
+    #     file_path = "images/" + sys.argv[1]
+    #     if not Path(file_path).exists():
+    #         print("Error: file does not exist")
+    #         return 1
+    #     else:
+    #         filename = sys.argv[1]
+    #
+    #     if len(sys.argv) == 2:
+    #         print("Run:", sys.argv[0], sys.argv[1], "sigma=1 threshold=80 by default")
 
 
 if __name__ == '__main__':
