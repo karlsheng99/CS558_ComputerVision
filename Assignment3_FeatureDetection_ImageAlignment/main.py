a6cfeebf89b8a8427596d565631264636f06514f
karlsheng99 <karlsheng99@gmail.com>
2021-11-01 20:02:44 -0400

    update file

--- Assignment3_FeatureDetection_ImageAlignment/main.py
+++ Assignment3_FeatureDetection_ImageAlignment/main.py
@@ -1,12 +1,12 @@
-from PIL import Image, ImageOps
+from PIL import Image
 import numpy as np
 import convolution
 import harris
 
 
 def main():
-    img = Image.open('AlignmentTwoViews/uttower_right.jpg')
-    matrix = np.array(ImageOps.grayscale(img))
+    img = Image.open('AlignmentTwoViews/uttower_right.jpg').convert('L')
+    matrix = np.array(img)
     r = harris.harris_detector(matrix, 1, 3000)
     rr = Image.fromarray(r)
     rr.show()
