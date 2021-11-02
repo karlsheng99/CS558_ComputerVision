cdc3d99e421f410fa931bb07a1fc7072fcf73c05
karlsheng99 <karlsheng99@gmail.com>
2021-11-02 18:26:18 -0400

    update hw3

--- Assignment3_FeatureDetection_ImageAlignment/main.py
+++ Assignment3_FeatureDetection_ImageAlignment/main.py
@@ -6,16 +6,18 @@ import matching
 
 
 def main():
-    img1 = Image.open('AlignmentTwoViews/uttower_left.jpg').convert('L')
-    img2 = Image.open('AlignmentTwoViews/uttower_right.jpg').convert('L')
-    matrix1 = np.array(img1)
-    matrix2 = np.array(img2)
-    r1 = harris.harris_detector(matrix1, 1, 3000)
-    r2 = harris.harris_detector(matrix2, 1, 3000)
-
-    x = matching.ssd(img1, img2, matrix1, matrix2, r1, r2)
-    print(x)
-
+    img1 = Image.open('AlignmentTwoViews/uttower_left.jpg')
+    img2 = Image.open('AlignmentTwoViews/uttower_right.jpg')
+    # img1 = img1.rotate(45, expand=True)
+    matrix1 = np.array(img1.convert('L'))
+    matrix2 = np.array(img2.convert('L'))
+    r1 = harris.harris_detector(matrix1, 1)
+    r2 = harris.harris_detector(matrix2, 1)
+    # rr1 = Image.fromarray(r1)
+    # rr1.show()
+    # rr2 = Image.fromarray(r2)
+    # rr2.convert('L').save('non-max2.png')
+    matching.patch_similarity_matching(img1, img2, matrix1, matrix2, r1, r2, 15)
 
 
 if __name__ == '__main__':
--- Assignment3_FeatureDetection_ImageAlignment/matching.py
+++ Assignment3_FeatureDetection_ImageAlignment/matching.py
@@ -1,4 +1,7 @@
+from PIL import Image
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
 
 def detect_points(source):
@@ -17,43 +20,91 @@ def detect_points(source):
     return points
 
 
-def ssd(source_image_1, source_image_2, source_matrix_1, source_matrix_2, harris_matrix_1, harris_matrix_2, window_size=3):
+def plot_matching_features(source_image_1, source_image_2, best_20_pairs, window_size, title):
+    new_img = Image.new('RGB', (source_image_1.width + source_image_2.width, source_image_1.height))
+    new_img.paste(source_image_1, (0, 0))
+    new_img.paste(source_image_2, (source_image_1.width, 0))
+
+    fig, ax = plt.subplots(1)
+    ax.imshow(new_img)
+
+    for pair in best_20_pairs:
+        y1, x1 = pair[0]
+        y2, x2 = pair[1]
+
+        rect_1 = patches.Rectangle((x1, y1), window_size, window_size, linewidth=1, edgecolor='r', facecolor='none')
+        rect_2 = patches.Rectangle((x2 + source_image_1.width, y2), window_size, window_size, linewidth=1, edgecolor='r', facecolor='none')
+
+        ax.add_patch(rect_1)
+        ax.add_patch(rect_2)
+
+        plt.plot([x1, x2 + source_image_1.width], [y1, y2], color='red', linewidth=1)
+
+    plt.title(title)
+    plt.show()
+
+
+def patch_similarity_matching(source_image_1, source_image_2, source_matrix_1, source_matrix_2, harris_matrix_1, harris_matrix_2, window_size=7):
     points_1 = detect_points(harris_matrix_1)
     points_2 = detect_points(harris_matrix_2)
     window_half_size = int(window_size / 2)
 
     # Keep tracking the coordinates of top 20 matching points
-    best_20_matches = []
+    best_20_ssd_pairs = []
+    best_20_ncc_pairs = []
 
     # Keep tracking the ssd value of top 20 matching points
     best_20_ssd = np.full(20, np.inf)
+    best_20_ncc = np.full(20, - np.inf)
 
     for point_1 in points_1:
         y1, x1 = point_1
+        if y1 < window_half_size or x1 < window_half_size or \
+                y1 > (len(source_matrix_1) - window_half_size - 1) or \
+                x1 > (len(source_matrix_1[0]) - window_half_size - 1):
+            break
+        window_1 = np.array(source_matrix_1[y1 - window_half_size: y1 + window_half_size + 1,
+                                            x1 - window_half_size: x1 + window_half_size + 1])
         for point_2 in points_2:
             y2, x2 = point_2
-
-            ssd_matrix = np.array((source_matrix_1[y1 - window_half_size: y1 + window_half_size + 1,
-                                                   x1 - window_half_size: x1 + window_half_size + 1] -
-                                   source_matrix_2[y2 - window_half_size: y2 + window_half_size + 1,
-                                                   x2 - window_half_size: x2 + window_half_size + 1]) ** 2)
-            ssd_value = ssd_matrix.sum()
-
-            if ssd_value >= best_20_ssd[19]:
+            if y2 < window_half_size or x2 < window_half_size or \
+                    y2 > (len(source_matrix_2) - window_half_size - 1) or \
+                    x2 > (len(source_matrix_2[0]) - window_half_size - 1):
                 break
-            else:
-                for i in range(20):
-                    if ssd_value < best_20_ssd[i]:
-                        best_20_ssd = np.insert(best_20_ssd, i, ssd_value)
-                        best_20_ssd = best_20_ssd[:-1]
+            window_2 = np.array(source_matrix_2[y2 - window_half_size: y2 + window_half_size + 1,
+                                                x2 - window_half_size: x2 + window_half_size + 1])
 
-                        best_20_matches.insert(i, [point_1, point_2])
-                        if len(best_20_matches) > 20:
-                            best_20_matches = best_20_matches[:-1]
+            ssd_value = np.sum((window_1 - window_2) ** 2)
 
-                        break
+            mean_1 = np.mean(window_1)
+            mean_2 = np.mean(window_2)
 
-    return best_20_matches
+            numerator = np.sum((window_1 - mean_1) * (window_2 - mean_2))
+            denominator = np.sqrt(np.sum((window_1 - mean_1) ** 2) * np.sum((window_2 - mean_2) ** 2))
 
+            ncc_value = numerator / denominator
 
+            for i in range(len(best_20_ssd)):
+                if ssd_value < best_20_ssd[i]:
+                    best_20_ssd = np.insert(best_20_ssd, i, ssd_value)
+                    best_20_ssd = best_20_ssd[:-1]
+
+                    best_20_ssd_pairs.insert(i, [point_1, point_2])
+                    if len(best_20_ssd_pairs) > len(best_20_ssd):
+                        best_20_ssd_pairs = best_20_ssd_pairs[:-1]
+
+                    break
+
+                if ncc_value > best_20_ncc[i]:
+                    best_20_ncc = np.insert(best_20_ncc, i, ncc_value)
+                    best_20_ncc = best_20_ncc[:-1]
+
+                    best_20_ncc_pairs.insert(i, [point_1, point_2])
+                    if len(best_20_ncc_pairs) > len(best_20_ncc):
+                        best_20_ncc_pairs = best_20_ncc_pairs[:-1]
+
+                    break
+
+    plot_matching_features(source_image_1, source_image_2, best_20_ssd_pairs, window_size, 'SSD')
+    plot_matching_features(source_image_1, source_image_2, best_20_ncc_pairs, window_size, 'NCC')
 
